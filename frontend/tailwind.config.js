027485c14e010c3a9f21e3d7fa6eb108d4fb3c2e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-01-25 10:54:25 +0100

    second

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}", // Indiquez les chemins de vos fichiers
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
