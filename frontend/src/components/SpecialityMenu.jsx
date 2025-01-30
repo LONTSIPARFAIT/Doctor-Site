9247f1fd0cc96a55189707ddf389dadf3bc800a2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-01-30 10:38:36 +0100

    second1

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { specialityData } from '../assets/assets'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { specialityData } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const SpecialityMenu = () => {
   return (
-    <div id='speciality'>
-      <h1 className=''>Rechercher par spécialité</h1>
-      <p className=''>Parcourez simplement notre vaste liste de médecins de confiance et planifiez votre rendez-vous sans tracas.</p>
-      <div className="">
-        {specialityData.map(() => {
-          <Link>
-            
+    <div id="speciality" className="flex flex-col items-center gap-4 py-16 text-gray-800" >
+      <h1 className="text-3xl font-medium">Rechercher par spécialité</h1>
+      <p className="sm:1/3 text-center text-sm ">
+        Parcourez simplement notre vaste liste de médecins de confiance <br /> et
+        planifiez votre rendez-vous sans tracas.
+      </p>
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+        {specialityData.map((item, index) => (
+          <Link onClick={()=>{scrollTo(0,0)}} key={index} to={`/doctors/${item.speciality}`} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500">
+            <img className="w-16 sm:w-24 mb-2" src={item.image} alt={item.speciality} />
+            <p className="">{item.speciality}</p>
           </Link>
-        })
-        }
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SpecialityMenu
+export default SpecialityMenu;
