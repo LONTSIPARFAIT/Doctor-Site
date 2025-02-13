33b59e7c64fc0ea1db9ab7fe00efffbcc43b8be7
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-13 15:09:12 +0100

    second3

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -93,7 +93,7 @@ export const doctors = [
         speciality: 'General physician',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: "Le Dr Richard James s'engage fermement à fournir des soins médicaux complets, en mettant l'accent sur la prévention, le diagnostic précis et un accompagnement personnalisé. En tant que médecin généraliste, il adopte une approche holistique, tenant compte des besoins physiques et émotionnels de ses patients. Le Dr James promeut des habitudes de vie saines et encourage des examens réguliers pour prévenir les maladies. Sa capacité à établir une communication ouverte permet à ses patients de se sentir en confiance et compris. Par son expertise et son dévouement, il s'efforce d'améliorer la santé et le bien-être de chacun.",
         fees: 50,
         address: {
             line1: '17th Cross, Richmond',
@@ -107,7 +107,7 @@ export const doctors = [
         speciality: 'Gynecologist',
         degree: 'MBBS',
         experience: '3 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: "Le Dr Emily Larson se distingue par son engagement envers la santé des femmes, en offrant des soins gynécologiques de haute qualité et une attention particulière à chaque patiente. En tant que gynécologue, elle adopte une approche empathique, créant un environnement sûr et accueillant pour discuter des préoccupations de santé. Le Dr Larson met l'accent sur la prévention des maladies, encourageant des examens réguliers et des pratiques de santé proactives. Sa compétence dans le diagnostic précoce et le traitement des affections féminines est remarquable. Grâce à sa communication ouverte et à son soutien, elle s'efforce d'améliorer la santé et le bien-être de ses patientes.",
         fees: 60,
         address: {
             line1: '27th Cross, Richmond',
@@ -121,7 +121,7 @@ export const doctors = [
         speciality: 'Dermatologist',
         degree: 'MBBS',
         experience: '1 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: "Le Dr Sarah Patel se distingue par son expertise en dermatologie, offrant des soins complets pour la santé de la peau, des cheveux et des ongles. En tant que dermatologue, elle adopte une approche personnalisée, écoutant attentivement les préoccupations de chaque patient. Le Dr Patel met l'accent sur la prévention des maladies cutanées, en conseillant sur des routines de soins adaptées et l'importance de la protection solaire. Sa compétence dans le diagnostic et le traitement des affections dermatologiques, qu'elles soient courantes ou complexes, est largement reconnue. Par sa communication claire et son soutien, elle s'efforce d'améliorer l'estime de soi et le bien-être de ses patients.",
         fees: 30,
         address: {
             line1: '37th Cross, Richmond',
@@ -135,7 +135,7 @@ export const doctors = [
         speciality: 'Pediatricians',
         degree: 'MBBS',
         experience: '2 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: "Le Dr Christopher Lee est un pédiatre dévoué, passionné par la santé et le bien-être des enfants. Il adopte une approche chaleureuse et rassurante, créant un environnement où les jeunes patients et leurs parents se sentent à l'aise. En tant que spécialiste des soins pédiatriques, le Dr Lee met l'accent sur la prévention et le développement sain, offrant des conseils sur la nutrition, l'activité physique et les examens réguliers. Sa capacité à établir une relation de confiance avec les enfants permet un diagnostic et un traitement efficaces. Grâce à son engagement envers l'éducation des parents et le suivi attentif, il s'efforce d'assurer un avenir sain pour chaque enfant.",
         fees: 40,
         address: {
             line1: '47th Cross, Richmond',
@@ -149,7 +149,7 @@ export const doctors = [
         speciality: 'Neurologist',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: "Le Dr Jennifer Garcia est neurologue, spécialisée dans le diagnostic et le traitement des troubles du système nerveux. Avec une approche attentive et compréhensive, elle s'efforce d'établir une communication claire avec ses patients, leur permettant de comprendre leur condition et les options de traitement disponibles. Le Dr Garcia met l'accent sur l'importance d'une évaluation précoce et d'une prise en charge proactive pour des affections telles que les migraines, l'épilepsie et les troubles neurodégénératifs. Grâce à son expertise et à sa passion pour la recherche, elle reste à la pointe des avancées en neurologie. Son engagement envers le bien-être de ses patients se traduit par un suivi personnalisé et un soutien constant dans leur parcours de santé.",
         fees: 50,
         address: {
             line1: '57th Cross, Richmond',
@@ -163,7 +163,7 @@ export const doctors = [
         speciality: 'Gastroenterologist',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: "Le Dr Andrew Williams est gastro-entérologue, spécialisé dans le diagnostic et le traitement des troubles digestifs. Avec une approche centrée sur le patient, il s'efforce de créer un environnement où ses patients se sentent à l'aise pour discuter de leurs préoccupations. Le Dr Williams met l'accent sur l'importance de la prévention, en encourageant des habitudes alimentaires saines et des examens réguliers. Son expertise s'étend à des conditions telles que le reflux gastro-œsophagien, le syndrome de l'intestin irritable et les maladies inflammatoires de l'intestin. Grâce à ses connaissances approfondies et à sa capacité à établir une relation de confiance, il est déterminé à améliorer la santé digestive de chacun de ses patients.",
         fees: 50,
         address: {
             line1: '57th Cross, Richmond',
@@ -177,7 +177,7 @@ export const doctors = [
         speciality: 'General physician',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: "Le Dr Christopher Davis se distingue par son engagement envers la santé globale de ses patients en tant que médecin généraliste. Il adopte une approche centrée sur le patient, créant un environnement accueillant où chacun se sent libre de partager ses préoccupations de santé. Le Dr Davis met l'accent sur la prévention, encourageant des examens réguliers et des choix de vie sains pour éviter les maladies. Sa compétence en matière de diagnostic et de traitement des affections courantes est remarquable. Grâce à sa communication claire et à son soutien constant, il s'efforce d’améliorer le bien-être et la qualité de vie de chacun de ses patients, en les accompagnant à chaque étape de leur parcours de santé.",
         fees: 50,
         address: {
             line1: '17th Cross, Richmond',
