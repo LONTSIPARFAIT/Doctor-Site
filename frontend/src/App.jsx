3a19c8075ea83a439276583e1d05381c07d776d6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-13 11:01:18 +0100

    second1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/my-appointments' element={<MyAppointments />} />
-        <Route path='/appointment/docId' element={<Appointment />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
 
       <Footer />
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,32 +1,69 @@
 import { useContext, useEffect, useState } from 'react'
-
-import { useParams } from "react-router-dom"
 import { AppContext } from "../context/AppContext";
+import { useParams } from 'react-router-dom';
+
+// const Appointment = () => {
+
+//   const {docId} = useParams();
+//   const {doctors} = useContext(AppContext);
+//   // console.log(docId);
+
+//   const [docInfo, setDocInfo] = useState(null);
+
+//   const fetchDocInfo = async () => {
+//     const docInfo = doctors.find(doc => doc._id === docId);
+//     setDocInfo(docInfo);
+//     console.log(docInfo);
+//   }
+
+//   useEffect(() => {
+//     fetchDocInfo();
+//   }, [doctors,docId])
+
+//   return (
+//     <div>
+//       {/* --------------------------- Doctor Detail */}
+//       <div className="">
+//         <div className="">
+//           {/* <img src={docInfo.image} alt="" /> */}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
 const Appointment = () => {
+    const { docId } = useParams();
+    const { doctors } = useContext(AppContext);
+    const [docInfo, setDocInfo] = useState(null);
 
-  const {docId} = useParams();
-  const {doctors} = useContext(AppContext);
-  // console.log(docId);
+    useEffect(() => {
+        const foundDocInfo = doctors.find(doc => doc._id === docId);
+        setDocInfo(foundDocInfo || null);
+    }, [doctors, docId]);
 
-  const [docInfo, setDocInfo] = useState(null);
+    // Vérifiez si docInfo est null avant de rendre les détails
+    if (!docInfo) {
+        return <p>Loading...</p>; // ou un message d'erreur si nécessaire
+    }
 
-  const fetchDocInfo = async () => {
-    const docInfo = doctors.find(doc => doc._id === docId);
-    setDocInfo(docInfo);
-    console.log(docInfo);
-  }
+    return (
+        <div>
+            {/* --------------------------- Doctor Detail */}
+            <div className="">
+                <div className="">
+                    <img src={docInfo.image} alt={docInfo.name} /> {/* Affichage de l'image */}
+                </div>
+                <h1>{docInfo.name}</h1>
+                <p>{docInfo.speciality}</p>
+                <p>{docInfo.degree}</p>
+                <p>{docInfo.experience} of experience</p>
+                <p>{docInfo.fees} USD</p>
+                <p>{docInfo.about}</p>
+                <p>{docInfo.address.line1}, {docInfo.address.line2}</p>
+            </div>
+        </div>
+    );
+};
 
-  useEffect(() => {
-    fetchDocInfo();
-  }, [doctors,docId])
-
-  return (
-    <div>
-      {/* --------------------------- Doctor Detail */}
-      
-    </div>
-  )
-}
-
-export default Appointment
+export default Appointment;
