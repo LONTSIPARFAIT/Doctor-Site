b00d0a6102c7ff219d0e68afa3e3b903ea4141f0
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-14 12:09:06 +0100

    secondday

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,6 +1,17 @@
 // import React from 'react'
 
+import { useContext, useEffect, useState } from "react"
+import { AppContext } from "../context/AppContext"
+
 const RelatedDoctors = () => {
+    const {doctors} = useContext(AppContext)
+
+    const [relDoc, setRelDocs] = useState([])
+
+    useEffect(() =>{
+
+    },[doctors,speciality,docId])
+
   return (
     <div>
       
