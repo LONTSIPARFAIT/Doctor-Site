58a2212ecb32fe687134efb7cf9edfb9e87fb292
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-25 13:32:15 +0100

    secondday1

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
         <div className={`${showMenu ? "fixed w-full" : "h-0 w-0"} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
           <div className="flex items-center justify-between px-5 py-6">
             <img className='' src={assets.logo} alt="" />
-            <img c onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+            <img onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
           </div>
           <ul>
             <NavLink>ACCEUIL</NavLink>
