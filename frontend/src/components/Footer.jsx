c6d1a0f13d8d612903374ebd52bcde45f1ffea53
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-25 13:35:11 +0100

    secondday1

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         {/* -----------Left section */}
         <div className="">
             <img src={assets.logo} alt="" className="mb-5 w-40 " />
-            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer an took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
 
         {/* -----------center section */}
