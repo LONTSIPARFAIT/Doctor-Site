8c37e4d927e87fc2514da4ec243743405cc5c4c8
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-27 13:02:47 +0100

    Backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
 
-express();
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
