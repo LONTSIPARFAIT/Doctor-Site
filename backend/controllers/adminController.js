9c743fa1679735afbc1053c47e4f26d0ce4c1335
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-28 21:49:16 +0100

    Backend2

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -8,7 +8,7 @@ const addDoctor = async (req, res) => {
         const imageFile = req.file;
 
         // veriication de tous les donne d'ajout d'un medecin
-        if (!name || !password || !specialty || !degree || !experience || !about) {
+        if (!name || !password || !specialty || !degree || !experience || !about || !fees || !address) {
             
         }
 
