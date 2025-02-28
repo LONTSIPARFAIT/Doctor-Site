50f3f1dac6e801484e51220d0ef1d88427afdab6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-28 18:16:04 +0100

    Backend2

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,12 +4,16 @@
 const addDoctor = async (req, res) => {
     try {
 
-        const { name, email, password, specialty, degree, experience, about, fees, adress } = req.body;
-    //     const doctor = new doctor({ name, email, password, specialty });
+        const { name, email, password, specialty, degree, experience, about, fees, address } = req.body;
+        const imageFile = req.file;
+
+        console.log({name, email, password, specialty, degree, experience, about, fees, address},imageFile);
+        
     //     await doctor.save();
     //     res.status(201).json({ message: 'Medecin ajouté avec succès' });
     } catch (error) {
-        
+        console.error('Erreur lors de l\'ajout du médecin:', error);
+        res.status(500).json({ message: 'Erreur du serveur' });
     }
 }
 
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,9 +1,9 @@
 import express from 'express';
-// import { addDoctor } from '../controllers/adminController';
-// import upload from '../middlewares/multer';
+import { addDoctor } from '../controllers/adminController';
+import upload from '../middlewares/multer';
 
 const adminRouter = express.Router();
 
-// adminRouter.post('/add-doctor', upload.single('image'), addDoctor);
+adminRouter.post('/add-doctor', upload.single('image'), addDoctor);
 
 export default adminRouter;
