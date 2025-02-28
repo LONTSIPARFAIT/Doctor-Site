8972d457c2861c7ed9d27b092382dba2c530e3ea
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-28 17:18:02 +0100

    Backend2

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -5,9 +5,9 @@ const addDoctor = async (req, res) => {
     try {
 
         const { name, email, password, specialty, degree, experience, about, fees, adress } = req.body;
-        const doctor = new doctor({ name, email, password, specialty });
-        await doctor.save();
-        res.status(201).json({ message: 'Medecin ajouté avec succès' });
+    //     const doctor = new doctor({ name, email, password, specialty });
+    //     await doctor.save();
+    //     res.status(201).json({ message: 'Medecin ajouté avec succès' });
     } catch (error) {
         
     }
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js'; 
+import adminRouter from './routes/doctorRoute.js';
 
 // app config
 const app = express();
 const port = process.env.PORT || 5000;
-connectDB();
+connectDB();    
 connectCloudinary();
 
 // middlewares
@@ -15,6 +16,9 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints
+app.use('/api/admin', adminRouter);
+// localhost:5000/api/admin/add-doctor
+
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
 });
