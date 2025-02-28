1b14513a49b6aa7eb8f66616b35e47b535282117
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-02-28 16:40:47 +0100

    Backend2

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,11 +3,12 @@
 // API pour ajouter un medecin
 const addDoctor = async (req, res) => {
     try {
-        const { name, email, password, specialty } = req.body;
-        const doctor = new Doctor({ name, email, password, specialty });
+
+        const { name, email, password, specialty, degree, experience, about, fees, adress } = req.body;
+        const doctor = new doctor({ name, email, password, specialty });
         await doctor.save();
         res.status(201).json({ message: 'Medecin ajouté avec succès' });
-    } catch (error) {   
-        res.status(500).json({ message: error.message });
+    } catch (error) {
+        
     }
 }
