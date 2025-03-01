429bf08fe9a368ec0114d37cbe1238a1041d2be2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-01 10:45:44 +0100

    Backend2

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -5,6 +5,7 @@ const authAdmin = async (req, res, next) => {
     try {
         
     } catch (error) {
-        
+        console.error('Erreur lors de la connexion de l\'administrateur:', error);
+        res.status(500).json({ message: 'Erreur du serveur' });
     }
 }
