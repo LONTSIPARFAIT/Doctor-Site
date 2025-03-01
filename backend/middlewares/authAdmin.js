af66d72d5fbc2dcc8108cd218e276cb8d67c2075
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-01 10:43:10 +0100

    Backend2

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -2,5 +2,9 @@ import jwt from "jsonwebtoken";
 
 // Middleware pour l'authentification de l'admin
 const authAdmin = async (req, res, next) => {
-
+    try {
+        
+    } catch (error) {
+        
+    }
 }
