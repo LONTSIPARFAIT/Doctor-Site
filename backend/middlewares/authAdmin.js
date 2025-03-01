80ab64f57e8426c8e9ad68d773b723a8feaa2b75
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-01 10:58:39 +0100

    Backend2

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -3,9 +3,25 @@ import jwt from "jsonwebtoken";
 // Middleware pour l'authentification de l'admin
 const authAdmin = async (req, res, next) => {
     try {
+
+        const {atoken} = req.headers;
+
+        if (!atoken) {
+            return res.json({success:false, message: 'Token manquant' });
+        }
+
+        const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
+
+        if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+            return res.json({success:false, message: 'Token invalide' });
+        }
+
+        next();
         
     } catch (error) {
         console.error('Erreur lors de la connexion de l\'administrateur:', error);
         res.status(500).json({ message: 'Erreur du serveur' });
     }
 }
+
+export default authAdmin;
