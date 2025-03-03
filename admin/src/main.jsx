6c97b96c6646fd05ec17cfdb9036bdb1a78f7689
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-03 12:06:48 +0100

    admin

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrouserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 import AdminContextProvider from './context/AdminContext.jsx'
 import DoctorContextProvider from './context/DoctorContext.jsx'
-import AppContext from './context/AppContext.jsx'
 import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrouserRouter>
+  <BrowserRouter>
     <AdminContextProvider>
       <DoctorContextProvider>
         <AppContextProvider>
@@ -17,5 +16,5 @@ createRoot(document.getElementById('root')).render(
         </AppContextProvider>
       </DoctorContextProvider>
     </AdminContextProvider>
-  </BrouserRouter>,
+  </BrowserRouter>,
 )
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets.js'
 
 const Login = () => {
+
+    const [state, setState] = useState('Admin')
+
   return (
-    <div>
-      Login
-    </div>
+    <form className=''>
+      <div className="">
+        <p className=""><span> {state} </span> Login</p>
+      </div>
+    </form>
   )
 }
 
