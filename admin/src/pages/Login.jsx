beab829ac6b55daf3fe08ed7d0a29e7f3043b512
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-03 12:11:56 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
-import { assets } from '../assets/assets.js'
+import React, { useState } from "react";
+import { assets } from "../assets/assets.js";
 
 const Login = () => {
-
-    const [state, setState] = useState('Admin')
+  const [state, setState] = useState("Admin");
 
   return (
-    <form className=''>
+    <form className="">
       <div className="">
-        <p className=""><span> {state} </span> Login</p>
+        <p className="">Connexion <span> {state} </span></p>
+        <div>
+          <p>Email</p>
+          <input type="email" required/>
+        </div>
+        <div>
+          <p>Password</p>
+          <input type="password" required/>
+        </div>
+        <button>Connexion</button>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
