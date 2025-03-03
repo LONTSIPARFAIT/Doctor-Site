531186780ce0e9c9b67b956fc02ae2487c70a866
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-03 11:58:34 +0100

    admin

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -6,13 +6,15 @@ import { BrouserRouter } from 'react-router-dom'
 import AdminContextProvider from './context/AdminContext.jsx'
 import DoctorContextProvider from './context/DoctorContext.jsx'
 import AppContext from './context/AppContext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrouserRouter>
     <AdminContextProvider>
       <DoctorContextProvider>
-        <AppContext></AppContext>
-        <App />
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
       </DoctorContextProvider>
     </AdminContextProvider>
   </BrouserRouter>,
