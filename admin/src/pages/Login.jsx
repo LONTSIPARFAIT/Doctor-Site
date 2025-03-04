0c148df28f4cac6a446b94532a8d24b4a1bc1c4c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-04 21:35:45 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -22,22 +22,22 @@ const Login = () => {
   );
 };
 
-  return (
-    <form action="" className="min-h-[80vh] flex items-center">
-      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
-        <p className="text-2xl font-semibold"> Connexion <span> {state} </span> </p>
+  // return (
+  //   <form action="" className="min-h-[80vh] flex items-center">
+  //     <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
+  //       <p className="text-2xl font-semibold"> Connexion <span> {state} </span> </p>
 
-        <div className="w-full">
-          <p className="">Email</p>
-          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="email"  />
-        </div>
-        <div className="w-full">
-          <p className="">Mot de Passe</p>
-          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password"  />
-        </div>
-        <button className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer"> Se Connecter </button>
-      </div>
-    </form>
-  )
+  //       <div className="w-full">
+  //         <p className="">Email</p>
+  //         <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="email"  />
+  //       </div>
+  //       <div className="w-full">
+  //         <p className="">Mot de Passe</p>
+  //         <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password"  />
+  //       </div>
+  //       <button className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer"> Se Connecter </button>
+  //     </div>
+  //   </form>
+  // )
 
 export default Login
