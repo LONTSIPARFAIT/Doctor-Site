6f597996e3f1a0bb10f85146db876d2e2e2bdf8a
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-04 21:28:00 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -45,13 +45,7 @@ const Login = () => {
     <form action="" className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
         <p className="text-2xl font-semibold"> {state === 'Sign Up' ? "Creer un Compte" : "Se Connecter"} </p>
-        <p className="">Please {state === 'Sign Up' ? "sign up" : "log in"} to book appointment</p>
-        {
-          state === 'Sign Up' && <div className="w-full">
-          <p className="">Nom Complet</p>
-          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" onChange={(e)=>setName(e.target.name)} value={name} />
-        </div>
-        }
+
         <div className="w-full">
           <p className="">Email</p>
           <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="email" onChange={(e)=>setName(e.target.name)} value={email} />
@@ -60,7 +54,7 @@ const Login = () => {
           <p className="">Mot de Passe</p>
           <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" onChange={(e)=>setPassword(e.target.name)} value={passsword} />
         </div>
-        <button className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer"> {state === 'Sign Up' ? "Creer Un Compte" : "Se Connecter"} </button>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer"> Se Connecter </button>
         {
           state === "Sign Up"
           ? <p>Vous avez déjà un compte? <span onClick={()=>setState('Login')} className="text-primary underline cursor-pointer">Vous Connecter Ici</span></p>
