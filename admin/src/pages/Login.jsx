54b426ac43805ef84598f543816776800cfcd525
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-04 21:10:43 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
 
   return (
     <form className="min-h-[80vh] flex items-center">
-      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
         <p className="">Connexion <span> {state} </span></p>
         <div>
           <p>Email</p>
