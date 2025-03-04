c9e8fbf105bbb7d7d66d1b1a6def03967462fe3e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-04 21:34:28 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -8,11 +8,11 @@ const Login = () => {
     <form className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
         <p className="text-2xl font-semibold">Connexion <span> {state} </span></p>
-        <div>
+        <div className="w-full">
           <p>Email</p>
           <input type="email" className="border border-zinc-300 rounded w-full p-2 mt-1" required/>
         </div>
-        <div>
+        <div className="w-full">
           <p>Password</p>
           <input type="password" className="border border-zinc-300 rounded w-full p-2 mt-1" required/>
         </div>
