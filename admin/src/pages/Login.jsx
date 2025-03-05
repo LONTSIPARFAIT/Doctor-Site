9f7f749d9800861b7b8796c51635af7efa8d2177
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-05 14:12:45 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   const [state, setState] = useState("Admin");
   
   const [email, setEmail] = useState('')
-  const [passwor]
+  const [password]
 
   return (
     <form className="min-h-[80vh] flex items-center">
