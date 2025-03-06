4cc14b9396cbedcb6688b28aa405acfdf2f720f8
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-06 14:20:58 +0100

    admin

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -6,7 +6,7 @@ const AdminContextProvider = (props) => {
 
   const [aToken,setAToken] = useState('')
 
-  const 
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
 
   const value = {
     aToken,setAToken
