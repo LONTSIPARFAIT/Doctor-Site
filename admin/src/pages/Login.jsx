e0b3d03c5bc1e2b21bb4a912699904b58660b0d5
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-06 14:03:29 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
         <button className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer">Connexion</button>
         {
           state === "Admin"
-          ? <p>Connexion de Medecin</p>
+          ? <p>Connexion de Medecin <span></span></p>
           : <p></p>
         } 
       </div>
