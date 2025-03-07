0bdf640e2e7830e6ad03a7c3607ff4fef42371df
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-07 23:02:50 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   
 
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl font-semibold m-auto">Connexion <span className="text-primary"> {state} </span></p>
         <div className="w-full">
