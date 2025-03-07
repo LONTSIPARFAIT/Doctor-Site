758658d19564fe0df0c55d2d48b47380cdfea7de
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-08 00:04:52 +0100

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const 
+  const {atoken}
   return (
     <div>
       <Login />
