e81ca716db8e6a5caa883d06b97c19b3febdfba6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-07 23:31:22 +0100

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   return (
     <div>
+      <Login />
       <Login />
     </div>
   )
