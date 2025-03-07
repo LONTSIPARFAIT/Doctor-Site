0c7173484ac846a553cbf105febed0bb72e5a110
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-08 00:06:48 +0100

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const {atoken}
+  const {atoken} =euseContext(AppCont)
   return (
     <div>
       <Login />
