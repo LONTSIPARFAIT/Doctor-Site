3662a30ffa40a8f1341c20f6cd80a2f6d430b0f2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-07 22:45:59 +0100

    admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,14 +1,12 @@
 import React, { useContext, useState } from "react";
-import { AdminContext } from "../context/AdminContext";
-// import { assets } from "../assets/assets.js";
+import { AdminContext } from "../context/AdminContext"; // Importation correcte
 
 const Login = () => {
   const [state, setState] = useState("Admin");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState(""); // Initialisation correcte
 
-  const { setAToken,backendUrl } = useContext(AdminContext);
-  
+  const { setAToken, backendUrl } = useContext(AdminContext); // Corrigez la faute de frappe ici
 
   return (
     <form className="min-h-[80vh] flex items-center">
@@ -16,22 +14,33 @@ const Login = () => {
         <p className="text-2xl font-semibold m-auto">Connexion <span className="text-primary"> {state} </span></p>
         <div className="w-full">
           <p>Email</p>
-          <input onClick={(e) => setEmail(e.target.value)} value={email} type="email" className="border border-[#DADADA] rounded w-full p-2 mt-1" required/>
+          <input 
+            onChange={(e) => setEmail(e.target.value)} // Utilisation de onChange au lieu de onClick
+            value={email} 
+            type="email" 
+            className="border border-[#DADADA] rounded w-full p-2 mt-1" 
+            required
+          />
         </div>
         <div className="w-full">
           <p>Password</p>
-          <input onClick={(e) => setPassword(e.target.value)} value={password} type="password" className="border border-[#DADADA] rounded w-full p-2 mt-1" required/>
+          <input 
+            onChange={(e) => setPassword(e.target.value)} // Utilisation de onChange au lieu de onClick
+            value={password} 
+            type="password" 
+            className="border border-[#DADADA] rounded w-full p-2 mt-1" 
+            required
+          />
         </div>
         <button className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer">Connexion</button>
         {
           state === "Admin"
-          ? <p>Connexion de Medecin? <span className="text-primary hover:underline cursor-pointer" onClick={()=>setState('Doctor')}>Cliquez Ici</span></p>
-          : <p>Connexion Admin? <span className="text-primary hover:underline cursor-pointer" onClick={()=>setState('Admin')}>Cliquez Ici</span></p>
+          ? <p>Connexion de Médecin? <span className="text-primary hover:underline cursor-pointer" onClick={() => setState('Doctor')}>Cliquez Ici</span></p>
+          : <p>Connexion Admin? <span className="text-primary hover:underline cursor-pointer" onClick={() => setState('Admin')}>Cliquez Ici</span></p>
         } 
       </div>
     </form>
   );
 };
 
-
-export default Login
+export default Login;
