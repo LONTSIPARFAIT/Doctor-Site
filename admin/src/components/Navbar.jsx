4bbb12f010d604a8ae1bb51b69102247b58b9335
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-09 20:47:21 +0100

    admin

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
   const  {aToken} = useContext(AdminContext);
   return (
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white' >
-      <div>
-        <img className='flex' src={assets.admin_logo} alt='' />
+      <div className='flex'>
+        <img className='' src={assets.admin_logo} alt='' />
         <p> { aToken ? 'Admin' : 'Doctor' } </p>
       </div>
       <button>Logout</button>
