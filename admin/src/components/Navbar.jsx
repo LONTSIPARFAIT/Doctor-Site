fe31e3b5acabd53782572d3c522cc0a640131960
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-09 20:45:09 +0100

    admin

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AdminContext } from '../context/AdminContext';
 const Navbar = () => {
   const  {aToken} = useContext(AdminContext);
   return (
-    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border' >
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b' >
       <div>
         <img src={assets.admin_logo} alt='' />
         <p> { aToken ? 'Admin' : 'Doctor' } </p>
