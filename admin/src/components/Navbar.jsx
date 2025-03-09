01cc1e81662b9a986a6c28ea5e392cbbb5af2258
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-09 20:56:37 +0100

    admin

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white' >
       <div className='flex items-center gap-2 text-sm'>
         <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt='' />
-        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-50' > { aToken ? 'Admin' : 'Doctor' } </p>
+        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600' > { aToken ? 'Admin' : 'Doctor' } </p>
       </div>
-      <button>Logout</button>
+      <button className='' >Logout</button>
     </div>
   )
 }
