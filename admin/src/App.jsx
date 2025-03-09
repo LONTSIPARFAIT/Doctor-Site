2eaf5c67d2c2ba7e3e972c7b6d97582d0151277b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-09 19:40:58 +0100

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from "../context/AdminContext";
+
 
 
 const App = () => {
