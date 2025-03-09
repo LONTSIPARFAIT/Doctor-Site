c66736accfb5645a067ed140a5f996eb136f8d53
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-09 20:05:20 +0100

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const { aToken } = useContext(AdminContext)
 
   return aToken ? (
-    <div className=''> 
+    <div className='bg-'> 
       <ToastContainer />
 
     </div>
