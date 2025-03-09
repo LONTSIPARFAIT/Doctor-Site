da1ec06f13e86abefb3cfdca83215d33dd52410b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-09 19:52:38 +0100

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,11 +3,12 @@ import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AppContext } from './context/AppContext';
+import { AdminContext } from './context/AdminContext';
 
 
 
 const App = () => {
-  const { aToken } = useContext()
+  const { aToken } = useContext(AdminContext)
 
   return aToken ? (
     <div>
