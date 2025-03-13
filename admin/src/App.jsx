3b5de030c16c62aa323e6e50e91875cb1e19baf5
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-13 21:38:03 +0100

    admin1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
       <div className='flex items-start'>
         <Sidebar />
         <Routes>
-          <Route path='' />
+          <Route path='/' />
         </Routes>
       </div>
       {/* end intrus */}
