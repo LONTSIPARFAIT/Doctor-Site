7d2cbb8893e36e2e6cc1ed4dee7b8af5ba640879
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-13 21:40:01 +0100

    admin1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
         <Sidebar />
         <Routes>
           <Route path='/' element={<></>} />
-          <Route path='/admin' element={<></>} />
+          <Route path='/admin-' element={<></>} />
         </Routes>
       </div>
       {/* end intrus */}
