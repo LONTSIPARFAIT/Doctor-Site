a00c2792cf177e4aa69a3a5413ada3b025632f86
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-13 21:25:32 +0100

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,7 @@ import { ToastContainer,  } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 
 
@@ -14,7 +15,9 @@ const App = () => {
     <div className='bg-[#F8F9FD]'> 
       <ToastContainer />
       <Navbar />
-      <div></div>
+      <div>
+        <Sidebar />
+      </div>
 
     </div>
   ) : (
