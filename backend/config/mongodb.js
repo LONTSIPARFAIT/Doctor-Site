4f0e2fd0ab969263ccc75bdc556f8c4d33d28181
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-14 11:24:56 +0100

    admin1

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,14 +2,18 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
 
-  mongoose.connection.on("connected",() =>console.log("Mongodb connectée avec success"))
+  mongoose.connection.on("connected", () => {
+    console.log("Mongodb connectée avec succès");
+  });
+
+  // mongoose.connection.on("connected",() =>console.log("Mongodb connectée avec success"))
 
   // await mongoose.connect(`${process.env.MONGODB_URI}/Prescripto`)
 
     // Connexion à la base de données locale
     await mongoose.connect(`${process.env.MONGODB_URI}/Prescripto`, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true, 
     });
 
 }
