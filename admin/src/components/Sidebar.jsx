ae64b657443d48ed3aaf30509654d57b8689e128
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-14 11:32:20 +0100

    admin1

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -29,7 +29,7 @@ export default function Sidebar() {
           
           <NavLink>
             <img src={assets.people_icon} alt="" />
-            <p>Dashboard</p>
+            <p>LIste des medecin</p>
           </NavLink>
 
 
