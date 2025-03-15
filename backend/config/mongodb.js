9cae3958da3350869331872e79dbd8fbc75346ed
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-15 11:24:21 +0100

    admin1

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,19 +1,24 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("MongoDB connectée avec succès");
-  });
+  try {
+    // Connexion à la base de données locale
+    await mongoose.connect(`${process.env.MONGODB_URI}/Prescripto`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  mongoose.connection.on("error", (error) => {
-    console.error(`Erreur de connexion à MongoDB: ${error.message}`);
-  });
+    mongoose.connection.on("connected", () => {
+      console.log("MongoDB connectée avec succès");
+    });
 
-  // Connexion à la base de données locale
-  await mongoose.connect(`${process.env.MONGODB_URI}/Prescripto`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    mongoose.connection.on("error", (error) => {
+      console.error(`Erreur de connexion à MongoDB: ${error.message}`);
+    });
+    
+  } catch (error) {
+    console.error(`Erreur lors de la connexion à MongoDB: ${error.message}`);
+  }
 };
 
 export default connectDB;
