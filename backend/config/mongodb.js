fb4e92592d4263a0bf3b2e060d581cf7fea96bd1
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-15 11:20:53 +0100

    admin1

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  // try {
+  try {
     // Connexion à la base de données locale
     await mongoose.connect(`${process.env.MONGODB_URI}/Prescripto`, {
       useNewUrlParser: true,
@@ -16,9 +16,9 @@ const connectDB = async () => {
     //   console.error(`Erreur de connexion à MongoDB: ${error.message}`);
     // });
     
-  // } catch (error) {
-  //   console.error(`Erreur lors de la connexion à MongoDB: ${error.message}`);
-  // }
+  } catch (error) {
+    console.error(`Erreur lors de la connexion à MongoDB: ${error.message}`);
+  }
 };
 
 export default connectDB;
