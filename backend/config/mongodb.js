4dfc1df149c75108bd2e1991103def87f6b7e7fb
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-15 12:00:01 +0100

    admin1

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,19 +1,43 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("MongoDB connectée avec succès");
-  });
+    // Événements de connexion
+    mongoose.connection.on("connected", () => {
+        console.log("MongoDB connectée avec succès");
+    });
 
-  mongoose.connection.on("error", (error) => {
-    console.error(`Erreur de connexion à MongoDB: ${error.message}`);
-  });
+    mongoose.connection.on("error", (error) => {
+        console.error(`Erreur de connexion à MongoDB: ${error.message}`);
+    });
 
-  // Connexion à la base de données locale
-  await mongoose.connect(`${process.env.MONGODB_URI}/Prescripto`, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-  });
+    try {
+        // Connexion à la base de données
+        await mongoose.connect(`${process.env.MONGODB_URI}/Prescripto`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch (error) {
+        console.error(`Erreur lors de la tentative de connexion à MongoDB: ${error.message}`);
+        process.exit(1); // Arrêtez le serveur en cas d'erreur
+    }
 };
 
 export default connectDB;
+
+// const connectDB = async () => {
+//   mongoose.connection.on("connected", () => {
+//     console.log("MongoDB connectée avec succès");
+//   });
+
+//   mongoose.connection.on("error", (error) => {
+//     console.error(`Erreur de connexion à MongoDB: ${error.message}`);
+//   });
+
+//   // Connexion à la base de données locale
+//   await mongoose.connect(`${process.env.MONGODB_URI}/Prescripto`, {
+//     // useNewUrlParser: true,
+//     // useUnifiedTopology: true,
+//   });
+// };
+
+// export default connectDB;
