da8dd5b6ca53ee2014e4febb423f799f06dc9b9c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-15 11:53:50 +0100

    admin1

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -10,11 +10,3 @@ const connectCloudinary = async () => {
 
 export default connectCloudinary;
 
-const connectCloudinary = async () => {
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET
-    });
-};
-
