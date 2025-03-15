188b7a6ba9a640996cbaf5a36770391ffd737e87
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-15 12:26:44 +0100

    admin1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
     <>
 
     {/* start intrus */}
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <div className='flex items-start'>
         <Sidebar />
         <Dashboard />
