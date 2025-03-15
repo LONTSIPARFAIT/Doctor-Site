0f36344e66168ed4d7cecd1983dfafa9456d07bb
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-15 11:56:38 +0100

    admin1

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,41 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js'; 
 import adminRouter from './routes/doctorRoute.js';
 
-// app config
+// App config
 const app = express();
 const port = process.env.PORT || 5000;
 
-// connexion de la base de données
-connectDB();    
-connectCloudinary();
+// Connexion à la base de données et à Cloudinary
+const startServer = async () => {
+    try {
+        await connectDB();    
+        await connectCloudinary();
+        console.log('Connected to MongoDB and Cloudinary');
+    } catch (error) {
+        console.error('Error connecting to services:', error);
+        process.exit(1); // Arrêtez le serveur en cas d'erreur
+    }
+};
 
-// middlewares
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// api endpoints
+// API endpoints
 app.use('/api/admin', adminRouter);
-// localhost:5000/api/admin/add-doctor
 
+// Route de base
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
 });
 
-// listener
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Listener
+startServer().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 });
