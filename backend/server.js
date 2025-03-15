5384b45e43ebef4a106f3966e4baa44e54817f98
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-15 09:28:04 +0100

    admin1

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import adminRouter from './routes/doctorRoute.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
-// connexion de la base 
+// connexion de la base de données
 connectDB();    
 connectCloudinary();
 
