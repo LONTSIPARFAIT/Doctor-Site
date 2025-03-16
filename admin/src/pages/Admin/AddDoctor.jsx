77d5cf59ff60449fdf6073fbbe083a7e53a1a34f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-16 15:11:09 +0100

    admin1

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -5,7 +5,7 @@ const AddDoctor = () => {
   return (
     <form className='m-5 w-full'>
 
-      {/* <p className='mb-6 text-lg font-medium'>Ajouter un Médecin</p> */}
+      <p className='mb-6 text-lg font-medium'>Ajouter un Médecin</p>
 
       {/* <div className="bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll">
         <div className="flex items-center gap-4 mb-8 text-gray-500">
