b58b62f80ff39f3dd9d52588f097bee181fa9191
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-16 13:49:53 +0100

    admin1

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ export default function Sidebar() {
 
   return (
 
-    <div className='min-h-screen bg-white'>
+    <div className='min-h-screen bg-white border-r'>
       {
         // aToken && <ul>
           <ul>
