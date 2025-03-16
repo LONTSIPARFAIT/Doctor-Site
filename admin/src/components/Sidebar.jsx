04395f180900cd674e8227a84a280bf6293fda96
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-16 14:02:11 +0100

    admin1

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ export default function Sidebar() {
       {
         // aToken && <ul className='text[#515151] mt-5'>
           <ul className='text[#515151] mt-5'>
-          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[primary]' : ''}`} to={'/admin-dashboard'}>
+          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[5f6FFF]' : ''}`} to={'/admin-dashboard'}>
             <img src={assets.home_icon} alt="" />
             <p>Dashboard</p>
           </NavLink>
