9f53e50c11be7a1bd7609e7c284512d741d0e0fe
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-16 15:17:51 +0100

    admin1

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -17,9 +17,9 @@ const AddDoctor = () => {
         </div>
 
         <div className="flex flex-col lg:flex-row items-start gap-10 text-gray-600">
-          <div className="w-full lg:">
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
             
-            <div className="">
+            <div className="flex-1 flex">
               <p>Your Name</p>
               <input type="text" placeholder='Name' required />
             </div>
