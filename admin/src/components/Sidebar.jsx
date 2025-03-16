a60833d372d68e640d324328e3e02a704833e6f2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-16 13:46:26 +0100

    admin1

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ export default function Sidebar() {
             <p>Ajouter un medecin</p>
           </NavLink>
           
-          <NavLink to={'/all-appointments'}>
+          <NavLink to={'/doctor-list'}>
             <img src={assets.people_icon} alt="" />
             <p>Liste des medecins</p>
           </NavLink>
