95f31abed32881c2c9e250f75dde473477d09500
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-16 15:38:45 +0100

    admin1

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -5,7 +5,7 @@ const AddDoctor = () => {
 
   const {docImg,setDocImg} = useState(false)
   const {name,setName} = useState('')
-  const {email,setName} = useState('')
+  const {email,setEmail} = useState('')
 
   return (
     <form className='m-5 w-full'>
