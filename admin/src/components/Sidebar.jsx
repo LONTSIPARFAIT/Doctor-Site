31476cc5071117b888a9d46b8673aa8db9b2bf6d
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-16 14:03:54 +0100

    admin1

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ export default function Sidebar() {
             <p>Dashboard</p>
           </NavLink>
 
-          <NavLink to={'/all-appointments'}>
+          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/all-appointments'}>
             <img src={assets.appointment_icon} alt="" />
             <p>Rendez-vous</p>
           </NavLink>          
