84f86439c2d4ca8338e8630df83d78d6e058a8d8
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-17 20:22:35 +0100

    admin1

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -87,6 +87,8 @@ const loginAdmin = async (req, res) => {
         try {
             const doctors = await doctorModel
         } catch (error) {
+            console.error('Erreur lors de la connexion de l\'administrateur:', error);
+            res.status(500).json({ message: 'Erreur du serveur' });
             
         }
     }
