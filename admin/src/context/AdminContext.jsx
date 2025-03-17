bfba8944642976229595fa3bfe8976b553f7dd7a
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-17 20:55:46 +0100

    admin1

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider = (props) => {
 
   const getAllDoctors = async () => {
     try {
-      const {data} = await axios.post(backendUrl + '/api/admin/all')
+      const {data} = await axios.post(backendUrl + '/api/admin/all-')
     } catch (error) {
       
     }
