4b08b7b1d4c806cd54e96be6367cf3bdc4c74000
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-18 20:55:57 +0100

    admin1

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -8,7 +8,7 @@ const changeAvailablity = async (req,res)=>{
 
         const docData = await doctorModel.findById(docId)
         await doctorModel.findByIdAndUpdate(docId, { available:!docData.available })
-        res.json()
+        res.json({success})
         
     } catch (error) {
         console.error('Erreur lors de la connexion du medecin:', error);
