94c0314c70f67fb037e62625943f5951d0daa53b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-18 21:06:33 +0100

    admin1

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -28,7 +28,7 @@ const AdminContextProvider = (props) => {
 
   const changeAvailablity = async (docId) =>{
     try {
-      const {data} = await axios.post(backendUrl + '/api/admin/change-availability', {}, {headers:{aToken}})
+      const {data} = await axios.post(backendUrl + '/api/admin/change-availability', {docId}, {headers:{aToken}})
     } catch (error) {
       toast.error(error.message)
     }
