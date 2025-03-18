52d675e3fd59bf9ecc149fccbfe4e71280251293
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-18 20:48:09 +0100

    admin1

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -4,6 +4,7 @@ const changeAvailablity = async (req,res)=>{
     try {
         
     } catch (error) {
-        
+        console.error('Erreur lors de la connexion de l\'administrateur:', error);
+        res.status(500).json({ message: 'Erreur du serveur' });
     }
 }
