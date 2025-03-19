519c3e14335e4c1b508e6dbec4612ac29559692e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-19 12:52:35 +0100

    admin1

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props)=>{
 
     const currencySymbol = "$"
-    const backendUrl = import
+    const backendUrl = import.metz
 
     const value = {
         doctors,
