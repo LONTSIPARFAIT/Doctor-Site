b41cacc19966c647decfc00f30a8c9ef0ce69279
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-19 13:02:01 +0100

    admin1

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -19,7 +19,7 @@ const AppContextProvider = (props)=>{
         try { 
             const {data} = await axios.get(backendUrl + '/api/doctor/list')
 
-            if (condition) {
+            if (data) {
                 
             }
         } catch (error) {
