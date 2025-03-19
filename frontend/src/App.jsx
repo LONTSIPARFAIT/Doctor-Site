6ec7419096d52af938c0b78e8c7ffe4fe3c7a894
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-19 13:10:53 +0100

    admin1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import
 
 function App() {
 
