aa21249eaa811a336827af0a076855c3ebf8ef39
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-19 13:03:05 +0100

    admin1

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ const AppContextProvider = (props)=>{
 
     const currencySymbol = "$"
     const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const
+    const [] = 
 
     const value = {
         doctors,
