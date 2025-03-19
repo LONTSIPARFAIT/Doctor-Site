378804a507216f300dbc1690de755be527f5198b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-19 13:20:39 +0100

    admin1

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -24,7 +24,7 @@ const AppContextProvider = (props)=>{
             if (data.success) {
                 setDoctors(data.doctors)
             }else{
-                toast.error(error)
+                toast.error(data)
             }
         } catch (error) {
             console.log(error);
