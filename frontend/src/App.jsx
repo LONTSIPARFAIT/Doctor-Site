2ce6a78d5464aafeb40deef71e1f133b563133fc
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-19 13:13:18 +0100

    admin1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer, toast } from 'react-toastify'
 
 function App() {
 
