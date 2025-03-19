45e02ad702e78963a4156c1b4716bb75eb4fb2aa
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-19 12:57:01 +0100

    admin1

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -15,7 +15,7 @@ const AppContextProvider = (props)=>{
         currencySymbol,
     }
 
-    const getDoctorData = 
+    const getDoctorData = async
 
     return (
         <AppContext.Provider value={value}>
